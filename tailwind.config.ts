33754e1d9d0201ce104baa05fdb395eebf500973
Shua-github <CHA_shua@outlook.com>
2025-05-18 20:03:57 +0800

    fix 圆角

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,11 +62,14 @@ const config: Config = {
   				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
+		borderRadius: {
+			none: "0",
+			sm: "0.125rem",
+			md: "0.375rem",
+			lg: "0.5rem",
+			xl: "1rem",
+			full: "9999px",
+		},
   		keyframes: {
   			'accordion-down': {
   				from: {
